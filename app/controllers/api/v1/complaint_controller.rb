297c694a499259fcb47ccf807193541dd3e1197c
Ronit Steinberg <ronit.steinberg@wework.com>
2018-07-18 16:28:09 +0300

    fix

--- app/controllers/api/v1/complaint_controller.rb
+++ app/controllers/api/v1/complaint_controller.rb
@@ -15,7 +15,7 @@ class Api::V1::ComplaintController < ApplicationController
     render json: Complaint.find(params[:id]).update!(params.permit(:name, :text, :asset_url, :session_id))
   end
 
-  def generate_upload_url(filename)
+  def generate_upload_url
     aws_client = Aws::S3::Client.new(
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
@@ -23,7 +23,7 @@ class Api::V1::ComplaintController < ApplicationController
 
     s3 = Aws::S3::Resource.new(client: aws_client)
     bucket = s3.bucket('bucket-name')
-    obj = bucket.object("#{filename}")
+    obj = bucket.object("#{params[:filename]}")
 
     url = obj.presigned_url(:put)
 
