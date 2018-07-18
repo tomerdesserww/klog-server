331b5411c6f11c8d42117478d0712f331e19ec07
Ronit Steinberg <ronit.steinberg@wework.com>
2018-07-18 16:59:32 +0300

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,12 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      namespace :api do
-        namespace :v1 do
-          get 'complaint/index'
-          get 'complaint/show'
-          get 'complaint/create'
-          get 'complaint/update'
+      resources :test
+      resources :complaint do
+        collection do
+          post :generate_upload_url
         end
       end
-    end
-  end
-  get 'complaint/index'
-  get 'complaint/show'
-  get 'complaint/create'
-  get 'complaint/update'
-  namespace :api do
-    namespace :v1 do
-      resources :test, :complaint
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
