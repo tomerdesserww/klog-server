3c97132b9f48a591a3c1531309d7f96aedf5ec21
Ronit Steinberg <ronit.steinberg@wework.com>
2018-07-18 20:04:52 +0300

    debug

--- app/controllers/api/v1/complaint_controller.rb
+++ app/controllers/api/v1/complaint_controller.rb
@@ -16,26 +16,26 @@ class Api::V1::ComplaintController < ApplicationController
   end
 
   def generate_upload_url
-    res = 'test'
-    begin
+    # res = 'test'
+    # begin
     aws_client = Aws::S3::Client.new(
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: ENV['AWS_REGION']
     )
-    rescue => ex
-      res = ex
-      return render json: { uploadUrl: res }
-    end
+    # rescue => ex
+    #   res = ex
+    #   return render json: { uploadUrl: res }
+    # end
 
 
+    s3 = Aws::S3::Resource.new(client: aws_client)
+    bucket = s3.bucket('klog-complaint-images')
+    obj = bucket.object("#{params[:filename]}")
 
-    # s3 = Aws::S3::Resource.new(client: aws_client)
-    # bucket = s3.bucket('klog-complaint-images')
-    # obj = bucket.object("#{params[:filename]}")
-    #
-    # url = obj.presigned_url(:put)
-    #
-    # render json: { uploadUrl: url }
-    render json: { uploadUrl: res }
+    url = obj.presigned_url(:put)
+
+    render json: { uploadUrl: url }
+    # render json: { uploadUrl: res }
   end
 end
