3f51b765e4c469bd7538f8cc70f0fed0031bb160
Ronit Steinberg <ronit.steinberg@wework.com>
2018-07-18 19:41:23 +0300

    debug

--- app/controllers/api/v1/complaint_controller.rb
+++ app/controllers/api/v1/complaint_controller.rb
@@ -16,17 +16,18 @@ class Api::V1::ComplaintController < ApplicationController
   end
 
   def generate_upload_url
-    aws_client = Aws::S3::Client.new(
-      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-    )
-
-    s3 = Aws::S3::Resource.new(client: aws_client)
-    bucket = s3.bucket('klog-complaint-images')
-    obj = bucket.object("#{params[:filename]}")
-
-    url = obj.presigned_url(:put)
-
-    render json: { uploadUrl: url }
+    # aws_client = Aws::S3::Client.new(
+    #   access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    #   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    # )
+    #
+    # s3 = Aws::S3::Resource.new(client: aws_client)
+    # bucket = s3.bucket('klog-complaint-images')
+    # obj = bucket.object("#{params[:filename]}")
+    #
+    # url = obj.presigned_url(:put)
+    #
+    # render json: { uploadUrl: url }
+    render json: { uploadUrl: 'happy' }
   end
 end
