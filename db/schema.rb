fc4fbfcd7008140f86c9fac9d6305851beba676a
Paz Aricha <paz@pazaricha.com>
2018-07-18 15:48:23 +0300

    extended comlaint

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_18_105148) do
+ActiveRecord::Schema.define(version: 2018_07_18_124312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "complaints", force: :cascade do |t|
     t.string "text"
-    t.string "image_url"
+    t.string "asset_url"
     t.string "session_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
