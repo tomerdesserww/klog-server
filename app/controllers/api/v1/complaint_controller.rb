5209a5ecd15d15dcff2049fe383a4a9ef55c6b23
Ronit Steinberg <ronit.steinberg@wework.com>
2018-07-18 15:45:26 +0300

    Add upload url generator

--- app/controllers/api/v1/complaint_controller.rb
+++ app/controllers/api/v1/complaint_controller.rb
@@ -14,4 +14,19 @@ class Api::V1::ComplaintController < ApplicationController
   def update
     render json: Complaint.find(params[:id]).update!(params.permit(:text, :image_url, :session_id))
   end
+
+  def generate_upload_url(filename)
+    aws_client = Aws::S3::Client.new(
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    )
+
+    s3 = Aws::S3::Resource.new(client: aws_client)
+    bucket = s3.bucket('bucket-name')
+    obj = bucket.object("#{filename}")
+
+    url = obj.presigned_url(:put)
+
+    render json: url
+  end
 end
